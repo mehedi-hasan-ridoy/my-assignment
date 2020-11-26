b64fefd902bbd38fda6c86d1b2b779add4d52c98
mehedi-hasan-ridoy <74454380+mehedi-hasan-ridoy@users.noreply.github.com>
2020-11-27 03:46:55 +0600

    Add files via upload

---
+++ project-4/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+
+        items:1,
+        margin:10,
+        loop:true,
+        dots:true,
+        autoplay:true,
+        autoplayTimeout:5000,
+       
+       
+    });
+  });
