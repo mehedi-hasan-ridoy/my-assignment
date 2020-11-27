3885118a0937bc52702cd5e645a04f0bda6e7153
mehedi-hasan-ridoy <74454380+mehedi-hasan-ridoy@users.noreply.github.com>
2020-11-27 16:25:25 +0600

    Add files via upload

---
+++ project-5-with-process-bar/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+
+        items:1,
+        margin:10,
+        loop:true,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:4000,
+        responsive:Object,
+       
+    });
+  });
