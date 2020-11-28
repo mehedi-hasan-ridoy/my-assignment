28e31e59f8576956cc9f3ac65b7573448595c7f2
mehedi-hasan-ridoy <74454380+mehedi-hasan-ridoy@users.noreply.github.com>
2020-11-29 01:45:45 +0600

    Add files via upload

---
+++ slick-slider -project/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('.slider-for').slick({
+      dots: true,
+     
+     
+      slidesToShow: 1,
+   
+      autoplay:true,
+      dotsclass:'slick-dots',
+      accessibility:false,
+      swipe:true,
+  });
+});
+       
+
